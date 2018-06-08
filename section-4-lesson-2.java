bf4b2d5b6aa8bfb4cdde634ed4dee8a80bcc3b9d
Al <afsm1995@live.com>
2018-06-07 22:06:03 -0600

    Morning Progress

---
+++ section-4-lesson-2.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int number = (10 + 5) + 2;
+        int number2 = 12;
+        int number3 = number * 6;
+
+        int total = 1000 - (number + number2 + number3);
+        System.out.println(number);
+        System.out.println("number");
+        System.out.println(total);
+    }
+}
